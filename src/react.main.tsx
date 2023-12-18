2b9dcc2c2e77d7dc15e1ba44f40845046ecec7cf
Raphael Melloni <raphael@melloni.de>
2023-12-18 16:16:22 +0100

    remove kolibri theme registration

--- src/react.main.tsx
+++ src/react.main.tsx
@@ -1,20 +1,22 @@
 import { register } from '@public-ui/components';
-import { DEFAULT } from '@public-ui/themes';
+import { BMF } from '@public-ui/themes';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-register(DEFAULT, [])
-	.then(() => {
-		const htmlElement: HTMLElement | null = document.querySelector<HTMLDivElement>('div#app');
-		if (htmlElement instanceof HTMLElement) {
-			const root = createRoot(htmlElement);
-			root.render(
-				<StrictMode>
-					<App />
-				</StrictMode>,
-			);
-		}
-	})
-	.catch(console.error);
+try {
+	await register(BMF, [])
+} catch (error) {
+	console.error(error)
+}
+
+const htmlElement: HTMLElement | null = document.querySelector<HTMLDivElement>('div#app');
+if (htmlElement instanceof HTMLElement) {
+	const root = createRoot(htmlElement);
+	root.render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+}
